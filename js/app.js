89539e7b57ee1708b798159aa1b1b8baf2570c45
karen <79118416+Karenjakins@users.noreply.github.com>
2021-05-21 23:04:00 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,3 +2,19 @@
 const tableData = data;
 // reference the HTML table using d3
 var tbody = d3.select("tbody");
+// Dynamic Tables
+function buildTable(data) {
+    // Clear any existing data
+    tbody.html("");
+    // Look through each object in the data and append 
+    data.forEach((dataRow) => {
+    // Append row to the table    
+    let row = tbody.append("tr");
+
+    //Loop through each field in the dataRow and add value to table cell (td)
+    Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+    }
+    );
+});
