e953865ce8a1c913f3e05c1abeb83b46a97eb168
karen <79118416+Karenjakins@users.noreply.github.com>
2021-05-22 21:36:11 -0400

    Updating code

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ function updateFilters() {
     // 4b. Save the value that was changed as a variable.
   let elementValue = changedElement.property("value");
     // 4c. Save the id of the filter that was changed as a variable.
-  let filterId = chngElement.attr("id");
+  let filterId = changedElement.attr("id");
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
   if (elementValue) {
